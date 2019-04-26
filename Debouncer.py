00162a07a79cb5d78e357ba3fd71c006c5440694
AlexGarrity <garrity2799@gmail.com>
2019-04-26 17:57:57 +0100

    Possibly a working debouncer

--- Debouncer.py
+++ Debouncer.py
@@ -1,5 +1,36 @@
 
 class Debouncer:
-    minimum_time
 
-    def __init__(self):
+    # The current state of the switch
+    current_state = False
+    # The current last signal received
+    current_signal = 0
+    last_signal = 0
+    delta_signal = 0
+    # The maximum and minimum signals the button can produce
+    max_signal = 0
+    half_signal = 0
+    min_signal = 0
+    # Permissible variance in the signal
+    stability_threshold = 0.1
+
+    def __init__(self, minimum_time):
+        self.minimum_time = minimum_time
+        # Open a bus to the button here
+        self.half_signal = self.max_signal / 2
+
+    def change_state(self):
+        if self.current_signal < self.half_signal:
+            self.current_signal = False
+        else:
+            self.current_signal = True
+
+    def get_state(self):
+        return self.current_state
+
+    def update(self):
+        self.last_signal = self.current_signal
+        # Get the current signal here
+        self.delta_signal = asb(self.last_signal - self.current_signal)
+        if self.delta_signal > self.half_signal:
+            self.change_state()

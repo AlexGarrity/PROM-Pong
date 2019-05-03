7c58eeaaa5eda54aca6b8203f2437b3ee02550a8
Jordan <jordan00789@gmail.com>
2019-05-03 16:50:04 +0100

    updated test file with correct gpio pins

--- ledtest.py
+++ ledtest.py
@@ -4,22 +4,31 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-leds = [21, 22, 26, 23, 27, 24, 28, 25]
+leds = [5, 6, 12, 13, 16, 19, 20, 26]
 
 num = 0
 
 for i in leds:
+    print("Setting up " + str(i))
     GPIO.setup(i, GPIO.OUT)
+    print("Turning on " + str(i))
+    GPIO.output(i, True)
+    time.sleep(0.5)
+    print("Turing off " + str(i))
+    GPIO.output(i, False)
+    time.sleep(1)
 
+
+#basic adder
 while True:
     num += 1
     tempNum = num
     for i in range(8, 0, -1):
         if tempNum - (2 ** i) >= 0:
-            print(str(tempNum) + " is bigger than or equal tdo " + str(2 ** i))
+            print(str(tempNum) + " is bigger than or equal to " + str(2 ** i))
             GPIO.output(leds[i - 1], True)
             tempNum -= (2 ** i)
         else:
             print(str(tempNum) + " is smaller than " + str(2 ** i))
             GPIO.output(leds[i - 1], False)
-    time.sleep(1)
+    time.sleep(0.25)

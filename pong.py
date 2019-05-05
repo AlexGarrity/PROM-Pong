95df3f0d6f1e945f3e4ff49441427de0de6a6446
AlexGarrity <garrity2799@gmail.com>
2019-05-06 00:21:17 +0100

    Refactor to turn pong.py into a class

--- pong.py
+++ pong.py
@@ -1,170 +1,228 @@
 import random
 import draw
 import constants
-import time
-
 import timing
 
 
-def start():
+class Ball:
+    position = [3, constants.height // 2]
+    direction_x = 1
+    direction_y = 0
 
-    timer = timing.Timing()
-
-    board = draw.Board(constants.length, constants.height)
-    board.prepare()
-    # bus.write_line(board.draw())
-
-    gametime = float(time.time())
-    balltime = gametime
-    updateTime = 1
-    timeToNextUpdate = 0
-    serving = False
-    serves = 0
-    playerTwoIsServing = False
-    playerOneBig = True
-    playerTwoBig = True
-    playerOneCanBig = True
-    playerTwoCanBig = True
-    gameOver = False
     speed = constants.speed[0]
+    time_to_next_move = 0.0
 
-    # currentNum = 0
+    def __init__(self):
+        # Reset speed and direction
+        self.set_position()
+        self.set_direction()
 
-    # Bat and ball default positions
-    playerOnePos = constants.height // 2
-    playerTwoPos = constants.height // 2
-    playerOneScore = 0
-    playerTwoScore = 0
-    ballPos = [3, constants.height // 2]
+    def set_speed(self, speed):
+        self.speed = speed
 
-    # ballMovement
-    ballDirectionX = 1
-    ballDirectionY = 0
+    def set_position(self, *position):
+        # If a position is given, set it to that
+        if position is not None:
+            self.position = [position[0], position[1]]
+        # If there's no position, assume it's being reset
+        else:
+            self.position = [3, constants.height // 2]
+
+    def set_direction(self, *direction):
+        # If a direction is given, set it to that
+        if direction is not None:
+            self.direction_x = direction[0]
+            self.direction_y = direction[1]
+        # If there's no direction, assume it's being reset
+        else:
+            self.direction_x = 1
+            self.direction_y = 0
+
+    def move(self, delta_time):
+        self.time_to_next_move -= delta_time
+        # If it's time for the ball to move
+        if self.time_to_next_move <= 0:
+            # Update the ball's position according to its direction
+            self.position[0] += self.direction_x
+            self.position[0] += self.direction_y
+            self.time_to_next_move = self.speed
 
 
-    board.clear()
-    board.updateScore(0)
-    board.updateScore(0, True)
-    board.updateBats(playerOnePos, playerTwoPos, playerOneBig, playerTwoBig)
-    board.updateBall(ballPos[0], ballPos[1])
-    # Infinite loop, maybe? Probably just stop it if a score reaches 10
-    # Remember, game logic is stored in this file. So we have the REAL pos of the bats/ball here.
-    while True:
-        # Our game clock. Our tickrate is around 13.3, meaning we update our game that many times per sec.
-        currentTime = float(time.time())
-        timer.update_time()
+class Bat:
+    position = constants.height // 2
+    score = 0
 
-        # Checking if we should tick
-        if currentTime - gametime >= 1 / constants.tickrate:
+    is_big = False
+    can_big = True
+    big_time = 2000
 
-            # The process is update game time, update bats (size first, THEN movement),
-            # update ball, check if ball is at edge.
-            # If yes, update score, if we won then yay. Otherwise reset positions, push "game time"
-            # forwards by 2 secs to freeze game. THEN we do serve logic until served
-            # regardless: clear board, initialise board, draw scores, draw bats, draw ball.
-            gametime = currentTime
-            # currentNum = (currentNum + 1) % 10
-            # bus.write_line(currentNum)
-            # board.updateScore(currentNum)
+    def __init__(self, *position):
+        if position is not None:
+            self.position = position[0]
 
-            # Emulating random button press for big bat
-            if random.randint(0, 250) == 9:
-                if playerOneBig or playerOneCanBig:
-                    playerOneBig = not playerOneBig
-                    playerOneCanBig = False
-            if random.randint(0, 250) == 9:
-                if playerTwoBig or playerTwoCanBig:
-                    playerTwoBig = not playerTwoBig
-                    playerTwoCanBig = False
+    def update(self, delta_time, movement):
+        self.big_time -= delta_time
+        if self.big_time <= 0 and self.is_big:
+            self.is_big = False
+        self.move(movement)
 
-            # Emulating random bat movement.
-            playerOneMove = random.randint(-1, 1)
-            playerTwoMove = random.randint(-1, 1)
+    def set_position(self, *position):
+        # If a position is given, set it to that
+        if position is not None:
+            self.position = position
+        # If there's no position, assume it's being reset
+        else:
+            self.position = constants.height // 2
 
-            playerOnePos += playerOneMove
-            playerTwoPos += playerTwoMove
+    # We wanna make sure our midpoint doesn't mean we're outside our game.
+    # So anything below 2 becomes 2, anything above 22 becomes 22. If big bats then 3 < x < 24
+    # bats are 1x3 despite image making them look 1x4. Regardless,
+    # this makes things easier. Take pos to be the midpoint.
+    def move(self, movement):
+        self.position += movement
 
-            # We wanna make sure our midpoint doesn't mean we're outside our game.
-            # So anything below 2 becomes 2, anything above 22 becomes 22. If big bats then 3 < x < 24
-            # bats are 1x3 despite image making them look 1x4. Regardless,
-            # this makes things easier. Take pos to be the midpoint.
-            if playerOnePos < (3 if playerOneBig else 2):
-                playerOnePos = (3 if playerOneBig else 2)
-            elif playerOnePos > (20 if playerOneBig else 22):
-                playerOnePos = (20 if playerOneBig else 22)
-            if playerTwoPos < (3 if playerTwoBig else 2):
-                playerTwoPos = (3 if playerTwoBig else 2)
-            elif playerTwoPos > (20 if playerTwoBig else 22):
-                playerTwoPos = (20 if playerTwoBig else 22)
+        if self.position < (3 if self.is_big else 2):
+            self.position = (3 if self.is_big else 2)
+        elif self.position > (20 if self.is_big else 22):
+            self.position = (20 if self.is_big else 22)
 
-            if serving:
-                ballPos = [constants.length - 4, playerTwoPos] if playerTwoIsServing else [3, playerOnePos]
+    def make_big(self):
+        if self.can_big:
+            self.is_big = True
+            self.can_big = False
 
-                # emulating random serve
-                if random.randrange(0, 100) == 9:
-                    ballDirectionY = random.randrange(-1, 1, 2)
-                    ballDirectionX = -1 if playerTwoIsServing else 1
-                    serving = False
-                    serves += 1
-                    if serves == 5:
-                        serves = 0
-                        playerTwoIsServing = not playerTwoIsServing
+    def check_hit(self, ball_y):
+        if self.is_big:
+            if -1 <= (self.position - ball_y) <= 1:
+                return True
+        else:
+            if -2 <= (self.position - ball_y) <= 3:
+                return True
+        return False
 
-            else:
-                # Move our ball.
-                if currentTime - balltime >= speed:
-                    balltime = currentTime
-                    ballPos = [ballPos[0] + ballDirectionX, ballPos[1] + ballDirectionY]
 
-                    # check if we hit the top or bottom, reverse ballDirY
-                    if ballPos[1] == 0 or ballPos[1] == constants.height - 1:
-                        ballDirectionY *= -1
+class Pong:
 
-                    # check if ball has hit bat, so calc next speed/direction
-                    if ballPos[0] == 2 or ballPos[0] == constants.length - 3:
-                        print("true")
-                        # nested if looks nicer
-                        # Can confirm
-                        if (-2 if playerOneBig else -1) <= playerOnePos - ballPos[1] <= (3 if playerOneBig else 1):
-                            print("hit right bat")
-                            # set our direction depending whether we hit top, middle, bottom (random right now)
-                            ballDirectionY = 1
-                            ballDirectionX *= -1
-                            speed = random.choice(constants.speed)
+    # Utility classes
+    timer = timing.Timing()
+    board = draw.Board(constants.length, constants.height)
 
-                        elif (-2 if playerTwoBig else -1) <= playerTwoPos - ballPos[1] <= (3 if playerTwoBig else 1):
-                            print("hit left bat")
+    # Gameplay classes
+    player_one = Bat()
+    player_two = Bat()
+    ball = Ball()
 
-                            ballDirectionY = -1
-                            ballDirectionX *= -1
-                            speed = random.choice(constants.speed)
+    # Game state vars
+    serving = True
+    playerTwoIsServing = False
+    serves = 0
+    gameOver = False
 
-            board.prepare()
-            board.updateBats(playerOnePos, playerTwoPos, playerOneBig, playerTwoBig)
-            board.updateBall(ballPos[0], ballPos[1])
-            # check if ball is at left/right edge, increase score, next serve.
-            if ballPos[0] == 0:
-                playerTwoScore += 1
-                serving = True
-                playerOnePos = constants.height // 2
-                playerTwoPos = constants.height // 2
-            elif ballPos[0] == constants.length - 1:
-                playerOneScore += 1
-                serving = True
-                playerOnePos = constants.height // 2
-                playerTwoPos = constants.height // 2
-            board.updateScore(playerOneScore)
-            board.updateScore(playerTwoScore, True)
-            # check if score is more than 9, win game.
-            if playerOneScore > 9:
-                board.updateWinner()
-                gameOver = True
-            elif playerTwoScore > 9:
-                gameOver = True
-                board.updateWinner(True)
-                # os.system("cls")
-            board.draw()
-            timeToNextUpdate = updateTime - (currentTime - gametime)
-            timer.wait_for_update()
-            if gameOver:
-                break
+    def start(self):
+        # Setup the board by putting the bats and ball in the right place
+        self.board.updateBats(self.player_one.position, self.player_two.position,
+                              self.player_one.is_big, self.player_two.is_big)
+
+        self.board.updateBall(self.ball.position[0], self.ball.position[1])
+
+        # Run the game loop until the game is over
+        while not self.gameOver:
+            self.game_loop()
+
+    def game_loop(self):
+        # Get the last frame time
+        self.timer.update_time()
+
+        # Simulate random presses of the 'make big' button
+        if random.randint(0, 250) == 9:
+            self.player_one.make_big()
+        if random.randint(0, 250) == 9:
+            self.player_two.make_big()
+
+        # Emulate random bat movement and update the big_time on the Bat
+        self.player_one.update(self.timer.deltaTime, random.randint(-1, 1))
+        self.player_two.update(self.timer.deltaTime, random.randint(-1, 1))
+
+        # If the current game state is serving, set up a serve
+        if self.serving:
+            self.setup_serve()
+
+            # Emulate a serve
+            if random.randint(0, 100) == 9:
+                if not self.playerTwoIsServing:
+                    self.ball.set_direction(1, random.randrange(-1, 1, 2))
+                else:
+                    self.ball.set_direction(-1, random.randrange(-1, 1, 2))
+
+                # Change the game state and the next serve
+                self.serving = False
+                self.serves += 1
+                if self.serves == 5:
+                    self.playerTwoIsServing = not self.playerTwoIsServing
+
+        # If the current game state is not serving (ie. playing)
+        else:
+            # Move the ball
+            self.ball.move(self.timer.deltaTime)
+            if self.ball.position[1] == 0 or self.ball.position[0] == constants.height - 1:
+                self.ball.direction_y *= -1
+
+            if self.ball.position[0] == 2 or self.ball.position[0] == constants.length - 3:
+                print("Ball may have hit bat")
+
+                if self.player_one.check_hit(self.ball.position[1]):
+                    print("Player one has hit the ball")
+                    self.reverse_ball_direction()
+                elif self.player_two.check_hit(self.ball.position[1]):
+                    print("Player two has hit the ball")
+                    self.reverse_ball_direction()
+
+        # Write the current game state to the board
+        self.board.prepare()
+        self.board.updateBats(self.player_one.position, self.player_two.position,
+                              self.player_one.is_big, self.player_two.is_big)
+
+        self.board.updateBall(self.ball.position[0], self.ball.position[1])
+
+        # If a player has scored, update their score and change the state to serving
+        if self.ball.position[0] == 0:
+            self.player_one.score += 1
+            self.set_serving()
+            self.board.updateScore(self.player_one.score)
+        elif self.ball.position[0] == constants.length - 1:
+            self.player_two.score += 1
+            self.set_serving()
+            self.board.updateScore(self.player_two.score, True)
+
+        # Check if either player has won the game
+        self.check_winner()
+
+        # Draw the board and wait until it needs to be drawn again
+        self.board.draw()
+        self.timer.wait_for_update()
+
+    def set_serving(self):
+        self.serving = True
+        self.player_one.set_position(None)
+        self.player_two.set_position(None)
+
+    def setup_serve(self):
+        if not self.playerTwoIsServing:
+            self.ball.set_position(3, self.player_one.position)
+        else:
+            self.ball.set_position(constants.length - 4, self.player_two.position)
+
+    def reverse_ball_direction(self):
+        self.ball.direction_y *= 1
+        self.ball.direction_x *= -1
+        self.ball.set_speed(random.choice(constants.speed))
+
+    def check_winner(self):
+        # If either player has a score greater than 9, they win
+        if self.player_one.score > 9:
+            self.gameOver = True
+            return True
+        elif self.player_two.score > 9:
+            self.gameOver = True
+            return True
+        return False

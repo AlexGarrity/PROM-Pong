719fab26df591286727f562d5f3187112f6b9414
Jordan <jordan00789@gmail.com>
2019-05-07 13:06:15 +0100

    forgot to sleep

--- glow.py
+++ glow.py
@@ -1,5 +1,5 @@
 from PyGlow import PyGlow
-
+import time
 glow = PyGlow()
 leds = [["White", 0], ["Blue", 0], ["Green", 0], ["Yellow", 0], ["Orange", 0], ["Red", 0]]
 order = [1, 3, 7, 15, 31, 63, 62, 60, 56, 48, 32, 0]
@@ -15,3 +15,4 @@ def score():
                 print("Turning off " + leds[led][0])
                 leds[led][1] = 0
             glow.color(leds[led][0], leds[led][1])
+        time.sleep(0.25)
